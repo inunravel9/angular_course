0fefe12f1a954638951d4fccc35d56aae6e54056
inunravel9 <inunravel9@ya.ru>
2026-04-13 18:13:52 +0700

    article count done

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const dialogOpenBtn = document.querySelector("#show-stats-btn");
 const dialog = document.querySelector("#stats-dialog");
 const dialogCloseBtn = document.querySelector("#close-stats");
+const articleSection = document.querySelectorAll(".blog-article");
+const articleCountSpan = document.querySelector("#article-count");
 
 dialogOpenBtn.onclick = () => {
   dialog.showModal();
@@ -9,3 +11,5 @@ dialogOpenBtn.onclick = () => {
 dialogCloseBtn.onclick = () => {
   dialog.close();
 };
+
+articleCountSpan.textContent=articleSection.length;
