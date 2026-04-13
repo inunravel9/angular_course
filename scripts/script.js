b7e03d363f8d70bd96e5d65b9e9017a8d5b8ad1b
inunravel9 <inunravel9@ya.ru>
2026-04-13 18:33:02 +0700

    module structure fot scripts added

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,3 @@
-const dialogOpenBtn = document.querySelector("#show-stats-btn");
-const dialog = document.querySelector("#stats-dialog");
-const dialogCloseBtn = document.querySelector("#close-stats");
-const articleCountSpan = document.querySelector("#article-count");
+import { initStatsDialog } from './modules/stats-dialog.js';
 
-const updateStats = () => {
-  const articles = document.querySelectorAll(".blog-article");
-  if (articleCountSpan) {
-    articleCountSpan.textContent = articles.length;
-  }
-};
-
-dialogOpenBtn.onclick = () => {
-  updateStats();
-  dialog.showModal();
-};
-
-dialogCloseBtn.onclick = () => {
-  dialog.close();
-};
-
-dialog.onclick = (event) => {
-  if (event.target === dialog) {
-    dialog.close();
-  }
-};
+initStatsDialog();
