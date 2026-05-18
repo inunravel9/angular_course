6c16407b26f7b2b0a096f8b0b4159b574ab077ab
inunravel9 <inunravel9@ya.ru>
2026-05-18 09:48:12 +0700

    bold copy

--- blog-app/webAppMyBlog/src/app/app.ts
+++ blog-app/webAppMyBlog/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './ui/components/header/header.ts'; // пути могут отличаться
+import { FooterComponent } from './ui/components/footer/footer.ts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // ОБЯЗАТЕЛЬНО добавьте их сюда
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('webAppMyBlog');
-}
+export class AppComponent { }
